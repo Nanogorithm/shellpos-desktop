8d42681c6135a0de2378755728c697488c5b8f7b
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2023-05-20 10:56:54 +0600

    Check

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,10 @@
 import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Card } from 'flowbite-react';
-import Pos from './Pages/POS/Pos';
 
 function Hello() {
   return (
     <div className="max-w-sm">
-      <Pos></Pos>
       <h1 className='text-sm text-slate-500'>Hello World</h1>
     </div>
   );
